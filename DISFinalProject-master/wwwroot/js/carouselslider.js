874ef445cab5a504aee07b56c7d61e4a12668b48
Shivanvitha Narala <shivanvitha@usf.edu>
2022-05-03 11:19:18 -0400

    updated git url

--- DISFinalProject-master/wwwroot/js/carouselslider.js
+++
@@ -1,7 +0,0 @@
-﻿var elems = document.querySelectorAll('.carousel-slider');
-var instances = M.Carousel.init(elems, {
-    duration: 200,
-    indicators: true,
-    fullWidth: true
-});
-setInterval(function () { instances[0].next(); }, 4000);
